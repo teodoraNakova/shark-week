f3b7ca66e40bb237760c77839617e4da16c8d56e
teodoraNakova <t_nakova_1991@abv.bg>
2018-04-10 21:29:09 +0300

    add UI of the calendar - using cadar

--- app/src/main/java/com/example/tedi/sharkweek/CalendarActivity.java
+++ app/src/main/java/com/example/tedi/sharkweek/CalendarActivity.java
@@ -1,13 +1,185 @@
 package com.example.tedi.sharkweek;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.util.Pair;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
 
-public class CalendarActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import io.github.memfis19.cadar.CalendarController;
+import io.github.memfis19.cadar.data.entity.Event;
+import io.github.memfis19.cadar.event.CalendarPrepareCallback;
+import io.github.memfis19.cadar.event.DisplayEventCallback;
+import io.github.memfis19.cadar.event.OnDayChangeListener;
+import io.github.memfis19.cadar.event.OnMonthChangeListener;
+import io.github.memfis19.cadar.internal.process.EventsProcessor;
+import io.github.memfis19.cadar.internal.process.EventsProcessorCallback;
+import io.github.memfis19.cadar.internal.utils.DateUtils;
+import io.github.memfis19.cadar.settings.ListCalendarConfiguration;
+import io.github.memfis19.cadar.settings.MonthCalendarConfiguration;
+import io.github.memfis19.cadar.view.ListCalendar;
+import io.github.memfis19.cadar.view.MonthCalendar;
+
+public class CalendarActivity extends AppCompatActivity implements CalendarPrepareCallback {
+
+    private MonthCalendar monthCalendar;
+    private ListCalendar listCalendar;
+
+    private List<Event> events = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        events.add(new EventModel());
+
+        monthCalendar = findViewById(R.id.monthCalendar);
+        listCalendar = findViewById(R.id.listCalendar);
+
+        MonthCalendarConfiguration.Builder builder = new MonthCalendarConfiguration.Builder();
+        ListCalendarConfiguration.Builder listBuilder = new ListCalendarConfiguration.Builder();
+
+        monthCalendar.setCalendarPrepareCallback(this);
+        listCalendar.setCalendarPrepareCallback(this);
+
+        builder.setDisplayPeriod(Calendar.MONTH, 3);
+        listBuilder.setDisplayPeriod(Calendar.MONTH, 3);
+        builder.setEventsProcessor(new MonthCustomProcessor());
+        listBuilder.setEventsProcessor(new ListCustomEventProcessor());
+
+        monthCalendar.prepareCalendar(builder.build());
+        listCalendar.prepareCalendar(listBuilder.build());
+
+        monthCalendar.setOnDayChangeListener(new OnDayChangeListener() {
+            @Override
+            public void onDayChanged(Calendar calendar) {
+                listCalendar.setSelectedDay(DateUtils.setTimeToMidnight((Calendar) calendar.clone()), false);
+            }
+        });
+        monthCalendar.setOnMonthChangeListener(new OnMonthChangeListener() {
+            @Override
+            public void onMonthChanged(Calendar calendar) {
+                listCalendar.setSelectedDay(DateUtils.setTimeToMonthStart((Calendar) calendar.clone()), false);
+            }
+        });
+
+        listCalendar.setOnDayChangeListener(new OnDayChangeListener() {
+            @Override
+            public void onDayChanged(Calendar calendar) {
+                monthCalendar.setSelectedDay(calendar, false);
+            }
+        });
+
+        listCalendar.setOnMonthChangeListener(new OnMonthChangeListener() {
+            @Override
+            public void onMonthChanged(Calendar calendar) {
+                monthCalendar.setSelectedDay(calendar, true);
+            }
+        });
     }
+
+    private Handler waitHandler = new Handler();
+
+    class ListCustomEventProcessor extends EventsProcessor<Pair<Calendar, Calendar>, List<Event>> {
+
+        public ListCustomEventProcessor() {
+            super(false, null, true);
+        }
+
+        @Override
+        protected void processEventsAsync(final Pair<Calendar, Calendar> target, final EventsProcessorCallback<Pair<Calendar, Calendar>, List<Event>> eventsProcessorCallback) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    final List<Event> events = new ArrayList<>();
+                    for (int i = 0; i < 5; ++i) {
+                        events.add(new EventModel());
+                    }
+                    waitHandler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            eventsProcessorCallback.onEventsProcessed(target, events);
+                        }
+                    }, 100);
+                }
+            }, 3000);
+        }
+    }
+
+    class MonthCustomProcessor extends EventsProcessor<Calendar, SparseArray<List<Event>>> {
+
+        public MonthCustomProcessor() {
+            super(false, null, true);
+        }
+
+        @Override
+        protected void processEventsAsync(final Calendar target, final EventsProcessorCallback<Calendar, SparseArray<List<Event>>> eventsProcessorCallback) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    List<Event> events = new ArrayList<>();
+                    for (int i = 0; i < 5; ++i) {
+                        events.add(new EventModel());
+                    }
+                    final SparseArray<List<Event>> calendarEvents = new SparseArray<>();
+
+                    Calendar temp = DateUtils.getCalendarInstance();
+                    for (Event event : events) {
+                        temp.setTime(event.getEventStartDate());
+
+                        if (!DateUtils.isSameMonth(temp, target)) continue;
+
+                        List<Event> tmpEvents = calendarEvents.get(temp.get(Calendar.DAY_OF_MONTH), new ArrayList<Event>());
+                        tmpEvents.add(event);
+
+                        calendarEvents.put(temp.get(Calendar.DAY_OF_MONTH), tmpEvents);
+                    }
+                    events.clear();
+
+                    waitHandler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            eventsProcessorCallback.onEventsProcessed(target, calendarEvents);
+                        }
+                    }, 100);
+                }
+            }, 3000);
+        }
+    }
+
+    @Override
+    public void onCalendarReady(CalendarController calendar) {
+        if (calendar == monthCalendar) {
+            monthCalendar.displayEvents(new ArrayList<>(events), new DisplayEventCallback<Calendar>() {
+                @Override
+                public void onEventsDisplayed(Calendar month) {
+
+                }
+            });
+        } else if (calendar == listCalendar) {
+            listCalendar.displayEvents(new ArrayList<>(events), new DisplayEventCallback<Pair<Calendar, Calendar>>() {
+                @Override
+                public void onEventsDisplayed(Pair<Calendar, Calendar> period) {
+
+                }
+            });
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        monthCalendar.releaseCalendar();
+        listCalendar.releaseCalendar();
+    }
+
 }
+
+
---
+++ app/src/main/java/com/example/tedi/sharkweek/EventModel.java
@@ -0,0 +1,134 @@
+package com.example.tedi.sharkweek;
+
+import java.util.Date;
+
+import io.github.memfis19.cadar.data.entity.Event;
+import io.github.memfis19.cadar.data.entity.property.EventProperties;
+
+public class EventModel implements Event {
+
+    private long id = System.currentTimeMillis();
+    private String title = "Event #" + String.valueOf(id);
+    private String description = "";
+    private Date originalStartDate = new Date();
+    private Date startDate = (Date) originalStartDate.clone();
+    private Date endDate = new Date();
+
+    public EventModel() {
+
+    }
+
+    public EventModel(Event event) {
+        this.id = event.getEventId();
+        this.title = event.getEventTitle();
+        this.description = event.getEventDescription();
+        this.originalStartDate = event.getOriginalEventStartDate();
+        this.startDate = event.getEventStartDate();
+        this.endDate = event.getEventEndDate();
+    }
+
+    @Override
+    public Long getEventId() {
+        return id;
+    }
+
+    @Override
+    public String getEventTitle() {
+        return title;
+    }
+
+    @Override
+    public String getEventDescription() {
+        return description;
+    }
+
+    @Override
+    public Date getOriginalEventStartDate() {
+        return originalStartDate;
+    }
+
+    @Override
+    public Date getEventStartDate() {
+        return startDate;
+    }
+
+    @Override
+    public Date getEventEndDate() {
+        return endDate;
+    }
+
+    @Override
+    public void setEventStartDate(Date startEventDate) {
+        this.startDate = startEventDate;
+    }
+
+    @Override
+    public void setEventEndDate(Date endEventDate) {
+        this.endDate = endEventDate;
+    }
+
+    @Override
+    public void setCalendarId(Long calendarId) {
+
+    }
+
+    @Override
+    public Long getCalendarId() {
+        return 0l;
+    }
+
+    @Override
+    public int getEventRepeatPeriod() {
+        return EventProperties.EVERY_WEEK;
+    }
+
+    @Override
+    public String getEventIconUrl() {
+        return "";
+    }
+
+    @Override
+    public Boolean isEditable() {
+        return false;
+    }
+
+    @Override
+    public Boolean isAllDayEvent() {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.getEventId() == 0) ? 0 : Long.valueOf(this.getEventId()).hashCode());
+        result = prime * result + ((this.getEventStartDate() == null) ? 0 : this.getEventStartDate().hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (((Object) this).getClass() != obj.getClass())
+            return false;
+
+        EventModel other = (EventModel) obj;
+
+        if (this.getEventId() == 0) {
+            if (other.getEventId() != 0)
+                return false;
+        } else if (getEventId() != other.getEventId())
+            return false;
+
+        if (this.getEventStartDate() == null) {
+            if (other.getEventStartDate() != null)
+                return false;
+        } else if (!getEventStartDate().equals(other.getEventStartDate()))
+            return false;
+
+        return true;
+    }
+}
